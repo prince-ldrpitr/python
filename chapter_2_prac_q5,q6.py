e3b400ab3a6c569851d02469810b8a4bbfd7f9a1
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-03-30 15:29:31 +0530

    5,6

---
+++ chapter_2_prac_q5,q6.py
@@ -0,0 +1,9 @@
+# q5
+n1=float(input("enter number n1 is:"))
+n2=float(input("enter number n2 is:"))
+print("the average of two number is:",(n1+n2)/2)
+
+
+# q6
+n=int(input("enter the number :"))
+print("the squre is number :",n**2)
