6e044707813dd9620f3d0dbe3ccb3cc348d9c9e4
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-03-30 15:26:49 +0530

    2

---
+++ chapter2_02.py
@@ -0,0 +1,32 @@
+# arithmetic operators
+a=33
+b=32
+print(a+b)
+# assigenment operators
+c=3
+c += 3
+print(c)
+
+# comaprison operators
+d = 5==4
+print(d)
+
+# logical operators
+# truth table of "or"
+print("true or false is:",True or False)
+print("true or false is:",True or True)
+print("true or false is:",False or True)
+print("true or false is:",False or False)
+
+# truth table of "and"
+print("true and false is:",True and False)
+print("true and false is:",True and True)
+print("true and false is:",False and True)
+print("true and false is:",False and False)
+
+print(not(False))
+# type casting
+p=33
+b=float(p)
+t=type(b)
+print(t)
