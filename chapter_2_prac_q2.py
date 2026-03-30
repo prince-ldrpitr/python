301f74edf541acc9e2dec9b9668d4c30cbae3d27
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-03-30 15:28:50 +0530

    2

---
+++ chapter_2_prac_q2.py
@@ -0,0 +1,15 @@
+# q2
+# a=65
+# b=32
+# print("the remainder is :",a%b)
+
+# q3
+
+# prince = input("the number is:")
+# t=type(prince)
+# print(t)
+
+# q4
+a=34
+b=80
+print("a is greater then b is:",a>b)
