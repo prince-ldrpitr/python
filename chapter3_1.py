e4215f5bf490b2d2fc4446f37b48c1ec93a12668
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-01 08:37:30 +0530

    1

---
+++ chapter3_1.py
@@ -0,0 +1,9 @@
+name = "dantani prince"
+shortname=name[0:3]
+print(shortname)
+print(name[0:3])
+print(len(name))
+print(name.endswith("nce"))
+print(name.startswith("pri"))
+print(name.capitalize())
+print(name.replace("prince","krishna"))
