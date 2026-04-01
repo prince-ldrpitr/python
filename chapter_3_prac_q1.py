6f97240d2181995706f4fecef6f5f44eb714ef64
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-01 08:38:49 +0530

    practice set of 3rd

---
+++ chapter_3_prac_q1.py
@@ -0,0 +1,15 @@
+# q1
+name = input("enter your name :")
+print(f"good morning {name}")
+
+# q2
+letter = '''  
+Dear <|Name|>, 
+You are selected! 
+<|Date|> 
+'''
+print(letter.replace("<|Name|>","prince").replace("<|Date|>","15 july 2007"))
+#  q3
+__name__ = "prince   "
+print(__name__.find("  "))
+
