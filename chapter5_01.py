ce0566cc2be5a6190f4b8eab33e48025ee0f0be4
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-02 19:49:21 +0530

    1

---
+++ chapter5_01.py
@@ -0,0 +1,17 @@
+student = {
+    "name": "Prince",
+    "age": 18,
+    "course": "IT"
+}
+print(student)
+print(student.items())
+print(student.keys())
+student.update({"name":"Krishna"})
+print(student)
+
+d = {1,6,3,33,25}
+print(d)
+print(len(d))
+d.remove(6)
+print(d)
+print(type(d))
