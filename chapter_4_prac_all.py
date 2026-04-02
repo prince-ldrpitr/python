414c815547721b0f9e26e829690476ce6db18fe5
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-02 19:47:45 +0530

    outputs

---
+++ chapter_4_prac_all.py
@@ -0,0 +1,28 @@
+# q1
+# fruits = []
+# f1 = input("enter fruit name :")
+# fruits.append(f1)
+# f2 = input("enter fruit name :")
+# fruits.append(f2)
+# f3 = input("enter fruit name :")
+# fruits.append(f3)
+# f4 = input("enter fruit name :")
+# fruits.append(f4)
+# f5 = input("enter fruit name :")
+# fruits.append(f5)
+# f6 = input("enter fruit name :")
+# fruits.append(f6)
+# f7 = input("enter fruit name :")
+# fruits.append(f7)
+# print(fruits)
+
+
+#4
+list = [33,33,33,33]
+print(sum(list))
+
+
+# q5
+a = (7, 0, 8, 0, 0, 9) 
+no=a.count(0)
+print(no)
