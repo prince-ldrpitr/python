92ec5e947dd01231a0a139a7ba923429cb18a075
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-03 13:21:56 +0530

    1

---
+++ chapter_5_prac_q1.py
@@ -0,0 +1,25 @@
+words = {
+    "prince" : "rajkumar",
+    "madad"  :  "help"
+}
+word = input("enter your word and get the meaning in english  :")
+print(words[word])
+# q2
+s = set()
+n= input("enter the number :")
+s.add(int(n))
+n= input("enter the number :")
+s.add(int(n))
+n= input("enter the number :")
+s.add(int(n))
+n= input("enter the number :")
+s.add(int(n))
+n= input("enter the number :")
+s.add(int(n))
+n= input("enter the number :")
+s.add(int(n))
+n= input("enter the number :")
+s.add(int(n))
+n= input("enter the number :")
+s.add(int(n))
+print(s)
