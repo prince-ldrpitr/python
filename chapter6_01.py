fe472a298168b35eeeaeb1fba7acfd0e3c303241
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-03 13:24:27 +0530

    1

---
+++ chapter6_01.py
@@ -0,0 +1,9 @@
+a = int(input("enter your age :"))
+if(a>=18):
+    print("you are 18 above")
+elif(a==0):
+    print("your age is zero")
+elif(a<0):
+    print("your age is negative")
+else:
+    print("you are 18 below")
