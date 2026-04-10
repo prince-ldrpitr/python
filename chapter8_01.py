8fe191cc1289df39e627aca8feb49a550213a69f
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-10 13:02:59 +0530

    1

---
+++ chapter8_01.py
@@ -0,0 +1,16 @@
+def fun():
+    print("today was a good day")
+
+fun()    
+
+
+def func():
+    a=float(input("enter the number:"))
+    b=float(input("enter the number:"))
+    c=float(input("enter the number:"))
+    return (a + b + c) / 3
+    
+    
+    
+
+print(func())
