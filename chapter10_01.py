a6af0110ca2a2a87d4782b9f4c193b17cd781712
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-13 08:58:53 +0530

    1

---
+++ chapter10_01.py
@@ -0,0 +1,11 @@
+class student:
+    name = "prince" 
+    enrollment = "24BEIT30033"
+    sem = "4th"                 
+
+
+krishna =  student()
+krishna.language = "python"
+krishna.name = "kumar"
+print(krishna.name,krishna.enrollment,krishna.sem)
+print(krishna.language)
