882d2487d9d021acb826ef3165060882ccc1cbfb
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-17 08:13:44 +0530

    1

---
+++ chapter12_01.py
@@ -0,0 +1,12 @@
+# Samajh lo Walrus Operator (:=) Python ka ek shortcut hai jo value assign bhi karta hai aur use bhi karta hai ek hi line me.
+
+# 🔹 Simple matlab
+
+# Normal tarike se:
+
+n = len("hello")
+print(n)
+
+# Walrus operator ke saath:
+
+print(n := len("hello"))
