56c092d2a59d65226adec280d39e937a4224a3da
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-23 10:34:53 +0530

    1

---
+++ chapter_10_prac_q1.py
@@ -0,0 +1,16 @@
+class programmer:
+
+
+    def __init__(self,name,salary,pin):
+        self.name = name
+        self.salary = salary
+        self.pin = pin
+
+
+p= programmer("prince",1200000,380005)
+
+print(p.name,p.salary,p.pin)
+j= programmer("janak",1200000,380004)
+
+print(j.name,j.salary,j.pin)
+
