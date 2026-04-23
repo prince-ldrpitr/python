05071b4be3f260ca00075412d8c8deba1a247079
dantani prince pintubhai <princekrishna9937@gmail.com>
2026-04-23 10:36:27 +0530

    2

---
+++ chapter_10_prac_q2.py
@@ -0,0 +1,14 @@
+class calculator:
+    def __init__(self,n):
+        self.n=n
+    def square(self):
+        print(f"the square is: {self.n*self.n}")
+    def cube(self):
+        print(f"the square is: {self.n*self.n*self.n}")
+    def squareroot(self):
+        print(f"the square is: {self.n**1/2}")
+
+a=calculator(4)
+a.square()
+a.cube()
+a.squareroot()
